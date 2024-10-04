8d028f261ce88a087187930f008453aa376ed159
Bhoomi Gohil <26293804+BhoomiGohil@users.noreply.github.com>
2024-10-04 13:04:57 -0400

    Changed folder name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import MainContent from "./Practice6/MainContent";
+import MainContent from "./js/MainContent";
 
 // ------------------------------------- Render Elements on browser -------------------------------------
 const rootElement = document.querySelector("#root");
